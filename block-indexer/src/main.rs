11463e3f840771c766618fb673a8b2636ec38478
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2025-02-21 12:57:56 +0000

    refactor(block-indexer): use Config from block-indexer binary crate

--- block-indexer/src/main.rs
+++ block-indexer/src/main.rs
@@ -1,36 +1,31 @@
 use anyhow::Result;
-use block_indexer::indexer::BlockIndexer;
-use block_indexer::store::CachedBlockStore;
-use common::rsk_indexer::RskIndexer;
-use common::shutdown_flag::ShutdownFlag;
-use dotenv::dotenv;
-use log::{error, info};
+use block_indexer::{indexer::BlockIndexer, store::CachedBlockStore};
+use common::{config::Config, rsk_indexer::RskIndexer, shutdown_flag::ShutdownFlag};
+use log::info;
 use rsk_provider::rpc::AlloyProvider;
-use std::env;
 
 fn main() -> Result<()> {
-    dotenv().expect("Failed to load .env file");
-    dotenv::from_filename("../.env").expect("Failed to load global .env file");
+    log4rs::init_file("config/log4rs.yml", Default::default())
+        .expect("Failed to load log4rs config");
 
-    log4rs::init_file("../log4rs.yml", Default::default()).expect("Failed to load log4rs config");
+    let config = Config::load("config/dev").expect("Failed to load config");
 
-    let store_path = env::var("STORE_PATH").expect("STORE_PATH not set in env");
-    let block_cache_size = env::var("BLOCK_CACHE_SIZE")
-        .expect("BLOCK_CACHE_SIZE not set in env")
-        .parse::<usize>()
-        .expect("BLOCK_CACHE_SIZE in env must be a number");
-    let store = CachedBlockStore::new(&format!("{}/blocks", store_path), block_cache_size)?;
+    let store = CachedBlockStore::new(
+        &format!("{}/blocks", config.indexer.storage.path),
+        config.indexer.cache.size.try_into().unwrap(),
+    )?;
 
     let shutdown_flag = ShutdownFlag::init();
 
-    let rsk_url = env::var("RSK_PROVIDER_URL").expect("RSK_PROVIDER_URL not set in env");
-    let alloy_provider = AlloyProvider::new(&rsk_url, shutdown_flag.clone())
-        .expect("Failed to create AlloyProvider");
+    let alloy_provider = AlloyProvider::new(&config.provider.rootstock.url, shutdown_flag.clone())
+        .expect("Failed to create AlloyProvider (unrecoverable)");
 
-    let initial_block_hash =
-        env::var("INITIAL_BLOCK_HASH").expect("INITIAL_BLOCK_HASH not set in env");
-
-    let indexer = BlockIndexer::new(store, alloy_provider, &initial_block_hash, shutdown_flag);
+    let indexer = BlockIndexer::new(
+        store,
+        alloy_provider,
+        &config.indexer.initial_block_hash,
+        shutdown_flag,
+    );
 
     indexer.run().inspect_err(|e| {
         error!("Unrecoverable error running block indexer: {:?}", e);
