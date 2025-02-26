8b204df9662757707f00eea6925046ef70ceb90c
F Javier Aguerri <javier.aguerri@rootstocklabs.com>
2025-02-26 12:48:31 +0100

    refactor indexer integration tests

--- common/src/test_utils/rsk.rs
+++ common/src/test_utils/rsk.rs
@@ -1,7 +1,7 @@
-use log::{debug, error, info, warn};
+use log::{debug, info};
 use std::sync::{
     atomic::{AtomicBool, Ordering},
-    Arc,
+    Arc, Mutex,
 };
 use std::{thread, time::Duration};
 
@@ -123,48 +123,25 @@ pub fn get_third_default_rsk_block() -> RskBlock {
 /// A stateless generator for fake RSK blocks that computes dynamic values (difficulty, timestamp,
 /// total difficulty and average block time) based on the block number. It has a built-in mechanism
 /// to handle generation of alternative blocks (to simulate reorganizations).`.
-///
-/// # Example
-///
-/// ```
-/// use common::test_utils::rsk::FakeBlockGenerator;
-/// use primitive_types::U256;
-///
-/// let mut generator = FakeBlockGenerator::new();
-/// let block = generator.generate_block(5, 0, false);
-///
-/// assert_eq!(block.number(), 5);
-/// ```
-///
-
 #[derive(Clone)]
 pub struct FakeBlockGenerator {
-    /// Base difficulty (e.g. 10^22).
     base_difficulty: U256,
-    /// Difficulty increment per block (e.g. 10^16).
     difficulty_increment: U256,
-    /// Base timestamp (Unix epoch, in seconds).
     base_timestamp: i64,
-    /// Average block time in seconds.
     avg_block_time: i64,
+    reorg_block_height: u64,
+    is_reorg: Arc<AtomicBool>,
 }
 
 impl FakeBlockGenerator {
-    /// Creates a new fake block generator with preset base values.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use common::test_utils::rsk::FakeBlockGenerator;
-    ///
-    /// let generator = FakeBlockGenerator::new();
-    /// ```
-    pub fn new() -> Self {
+    pub fn new(reorg_block_height: u64, is_reorg: Arc<AtomicBool>) -> Self {
         Self {
             base_difficulty: U256::from_dec_str("10000000000000000000000").unwrap(),
             difficulty_increment: U256::from_dec_str("10000000000000000").unwrap(),
             base_timestamp: 1514980800,
             avg_block_time: 30,
+            reorg_block_height: reorg_block_height,
+            is_reorg: is_reorg,
         }
     }
 
@@ -200,29 +177,14 @@ impl FakeBlockGenerator {
     }
 
     /// Generates a fake RSK block for the given block height.
-    ///
-    /// The block's parent hash is calculated as `generate_hash(height-1)`.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use common::test_utils::rsk::FakeBlockGenerator;
-    ///
-    /// let mut generator = FakeBlockGenerator::new();
-    /// let block = generator.generate_block(100, 0, true);
-    /// // The block height should be set as provided.
-    /// assert_eq!(block.number(), 100);
-    /// // The parent hash should equal the hash for block 99.
-    /// let expected_parent_hash = "0xef2310cd0c172059feb3c382c56acfbc8127222d4d2cc51b78db3019ce1a83f6";
-    /// assert_eq!(expected_parent_hash, block.parent());
-    /// ```
-    pub fn generate_block(&self, height: u64, reorg_block_height: u64, is_reorg: bool) -> RskBlock {
+    pub fn generate_block(&self, height: u64) -> RskBlock {
+        let is_reorg = self.is_reorg.load(Ordering::SeqCst);
         let parent_hash = if height == 0 {
             "0x0000000000000000000000000000000000000000000000000000000000000000".to_string()
         } else {
             self.generate_hash(
                 height - 1,
-                if (height > reorg_block_height) && is_reorg {
+                if (height > self.reorg_block_height) && is_reorg {
                     "alt"
                 } else {
                     ""
@@ -231,7 +193,7 @@ impl FakeBlockGenerator {
         };
         let block_hash = self.generate_hash(
             height,
-            if (height >= reorg_block_height) && is_reorg {
+            if (height >= self.reorg_block_height) && is_reorg {
                 "alt"
             } else {
                 ""
@@ -256,174 +218,162 @@ impl FakeBlockGenerator {
     }
 }
 
-pub fn set_provider_expect_get_block_by_hash(
-    mock: &mut MockRskProvider,
+pub struct MockRskProviderHandler {
+    provider: Arc<Mutex<MockRskProvider>>,
+    generator: FakeBlockGenerator,
     is_reorg: Arc<AtomicBool>,
-    expected_hash_string: String,
-    generator: &FakeBlockGenerator,
-    block_height: u64,
-    block_height_reorg_from: u64,
-) {
-    let generator_clone = generator.clone();
-    mock.expect_get_block_by_hash()
-        .with(mockall::predicate::eq(expected_hash_string))
-        .returning(move |_hash| {
-            let reorg_active = is_reorg.load(Ordering::SeqCst);
-            let reorg_from = if reorg_active {
-                block_height_reorg_from
-            } else {
-                0
-            };
-            Ok(Some(generator_clone.generate_block(
-                block_height,
-                reorg_from,
-                reorg_active,
-            )))
-        });
-}
-
-pub fn set_provider_expect_get_best_block(
-    mock: &mut MockRskProvider,
-    is_reorg: Arc<AtomicBool>,
-    generator: &FakeBlockGenerator,
-    block_height: u64,
-    block_height_reorg_from: u64,
-) {
-    let generator_clone = generator.clone();
-    mock.expect_get_best_block().returning(move || {
-        let reorg_active = is_reorg.load(Ordering::SeqCst);
-        let reorg_from = if reorg_active {
-            block_height_reorg_from
-        } else {
-            0
-        };
-        Ok(generator_clone.generate_block(block_height, reorg_from, reorg_active))
-    });
-}
-
-pub fn set_provider_expect_get_block_by_number(
-    mock: &mut MockRskProvider,
-    generator: &FakeBlockGenerator,
-    valid_range: std::ops::RangeInclusive<u64>,
-) {
-    set_provider_expect_get_block_by_number_generic(mock, generator, valid_range, |_| {});
-}
-
-pub fn set_provider_expect_get_block_by_number_with_shutdown_at_block(
-    mock: &mut MockRskProvider,
-    generator: &FakeBlockGenerator,
-    shutdown_flag: &ShutdownFlag,
-    valid_range: std::ops::RangeInclusive<u64>,
-    block_height_at_shutdown: u64,
-) {
-    let shutdown_flag_clone = shutdown_flag.clone();
-    set_provider_expect_get_block_by_number_generic(mock, generator, valid_range, move |height| {
-        if height == block_height_at_shutdown {
-            shutdown_flag_clone.set(true);
-        }
-    });
-}
-
-pub fn set_provider_expect_get_block_by_number_generic<F>(
-    mock: &mut MockRskProvider,
-    generator: &FakeBlockGenerator,
-    valid_range: std::ops::RangeInclusive<u64>,
-    callback: F,
-) where
-    F: Fn(u64) + Send + Sync + 'static,
-{
-    let generator_clone = generator.clone();
-    mock.expect_get_block_by_number().returning(move |height| {
-        if valid_range.contains(&height) {
-            callback(height);
-            Ok(Some(generator_clone.generate_block(height, 0, false)))
-        } else {
-            Ok(None)
-        }
-    });
-}
-
-pub fn set_provider_expect_get_block_by_number_with_reorg(
-    mock: &mut MockRskProvider,
-    is_reorg: Arc<AtomicBool>,
-    generator: &FakeBlockGenerator,
-    valid_range: std::ops::RangeInclusive<u64>,
-    block_height_reorg_happens_at: u64,
-    block_height_reorg_from: u64,
-) {
-    let generator_clone = generator.clone();
-    mock.expect_get_block_by_number().returning(move |height| {
-        if valid_range.contains(&height) {
-            if height == block_height_reorg_happens_at {
-                is_reorg.store(true, Ordering::SeqCst);
-                info!(
-                    "Reorg initiated at block height {} with hash {}",
-                    height,
-                    generator_clone.generate_hash(height, "alt")
-                );
-            }
-            let reorg_active = is_reorg.load(Ordering::SeqCst);
-            let reorg_from = if reorg_active {
-                block_height_reorg_from
-            } else {
-                0
-            };
-            Ok(Some(generator_clone.generate_block(
-                height,
-                reorg_from,
-                reorg_active,
-            )))
-        } else {
-            Ok(None)
-        }
-    });
-}
-
-pub fn set_provider_expect_subscribe_blocks(
-    mock: &mut MockRskProvider,
-    is_reorg: Arc<AtomicBool>,
-    generator: &FakeBlockGenerator,
-    shutdown_flag: &ShutdownFlag,
-    block_height_reorg_from: u64,
-    block_height_subscription_init: u64,
+    has_subscribed: Arc<AtomicBool>,
+    shutting_down: ShutdownFlag,
+    block_height_backward_sync_init: u64,
+    block_height_backward_sync_max: u64,
     block_height_subscription_max: u64,
+    block_height_reorg_happens_at: u64,
     delay_between_blocks_subscription: u64,
-) {
-    let generator_clone = generator.clone();
-    let shutdown_flag_clone = shutdown_flag.clone();
-    let mut height_subscr_counter = block_height_subscription_init;
-    mock.expect_subscribe_blocks().returning({
-        move |_shutdown_flag| {
-            let mut mock_sub = MockRskSubscription::<RskBlock>::new();
-            let generator_clone = generator_clone.clone();
-            let is_reorg_clone = is_reorg.clone();
-            let shutdown_flag_clone = shutdown_flag_clone.clone();
-            mock_sub.expect_next().returning({
-                move || {
-                    let reorg_active = is_reorg_clone.clone().load(Ordering::SeqCst);
-                    thread::sleep(Duration::from_millis(delay_between_blocks_subscription));
-                    let block = if reorg_active {
-                        generator_clone.generate_block(
+}
+
+impl MockRskProviderHandler {
+    pub fn new(
+        provider: Arc<Mutex<MockRskProvider>>,
+        generator: &FakeBlockGenerator,
+        is_reorg: Arc<AtomicBool>,
+        shutting_down: ShutdownFlag,
+        block_height_backward_sync_init: u64,
+        block_height_backward_sync_max: u64,
+        block_height_subscription_max: u64,
+        block_height_reorg_happens_at: u64,
+        delay_between_blocks_subscription: u64,
+    ) -> Self {
+        Self {
+            provider,
+            generator: generator.clone(),
+            is_reorg,
+            has_subscribed: Arc::new(AtomicBool::new(false)),
+            shutting_down: shutting_down,
+            block_height_backward_sync_init,
+            block_height_backward_sync_max,
+            block_height_subscription_max,
+            block_height_reorg_happens_at,
+            delay_between_blocks_subscription,
+        }
+    }
+
+    pub fn set_provider_expect_get_block_by_hash(
+        &mut self,
+        expected_hash_string: String,
+        block_height: u64,
+    ) {
+        let generator = self.generator.clone();
+        self.provider
+            .lock()
+            .unwrap()
+            .expect_get_block_by_hash()
+            .with(mockall::predicate::eq(expected_hash_string))
+            .returning(move |_hash| Ok(Some(generator.generate_block(block_height))));
+    }
+
+    pub fn set_provider_expect_get_best_block(&mut self) {
+        let generator = self.generator.clone();
+        let has_subscribed = self.has_subscribed.clone();
+        let block_height_subscription_max = self.block_height_subscription_max;
+        let block_height_backward_sync_max = self.block_height_backward_sync_max;
+        self.provider
+            .lock()
+            .unwrap()
+            .expect_get_best_block()
+            .returning(move || {
+                let block_height = if has_subscribed.load(Ordering::SeqCst) {
+                    block_height_subscription_max
+                } else {
+                    block_height_backward_sync_max
+                };
+                Ok(generator.generate_block(block_height))
+            });
+    }
+
+    pub fn set_provider_expect_get_block_by_number(
+        &mut self,
+        simul_reorg: bool,
+        simul_shutdown: Option<u64>,
+    ) {
+        let generator = self.generator.clone();
+        let block_height_backward_sync_init = self.block_height_backward_sync_init;
+        let block_height_backward_sync_max = self.block_height_backward_sync_max;
+        let block_height_subscription_max = self.block_height_subscription_max;
+        let block_height_reorg_happens_at = self.block_height_reorg_happens_at;
+        let has_subscribed = self.has_subscribed.clone();
+        let is_reorg = self.is_reorg.clone();
+        let shutting_down = self.shutting_down.clone();
+        self.provider
+            .lock()
+            .unwrap()
+            .expect_get_block_by_number()
+            .returning(move |height| {
+                let mut valid_range =
+                    block_height_backward_sync_init..block_height_backward_sync_max;
+                if has_subscribed.load(Ordering::SeqCst) {
+                    valid_range = block_height_backward_sync_init..block_height_subscription_max;
+                }
+                if valid_range.contains(&height) {
+                    if let Some(shutdown_height) = simul_shutdown {
+                        if height == shutdown_height {
+                            shutting_down.set(true);
+                        }
+                    }
+                    if simul_reorg && height == block_height_reorg_happens_at {
+                        is_reorg.store(true, Ordering::SeqCst);
+                        info!(
+                            "Reorg initiated at block height {} with hash {}",
+                            height,
+                            generator.generate_hash(height, "alt")
+                        );
+                    }
+                    Ok(Some(generator.generate_block(height)))
+                } else {
+                    Ok(None)
+                }
+            });
+    }
+
+    pub fn set_provider_expect_subscribe_blocks(&mut self, simul_reorg: bool) {
+        let block_height_reorg_happens_at = self.block_height_reorg_happens_at;
+        let is_reorg = self.is_reorg.clone();
+        let generator = self.generator.clone();
+        let shutting_down = self.shutting_down.clone();
+        let mut height_subscr_counter = self.block_height_backward_sync_max + 1;
+        let has_subscribed = self.has_subscribed.clone();
+        let delay_between_blocks_subscription = self.delay_between_blocks_subscription;
+        let block_height_subscription_max = self.block_height_subscription_max;
+        self.provider
+            .lock()
+            .unwrap()
+            .expect_subscribe_blocks()
+            .returning(move |_shutdown_flag| {
+                let mut mock_sub = MockRskSubscription::<RskBlock>::new();
+                let generator = generator.clone();
+                let shutting_down = shutting_down.clone();
+                let is_reorg = is_reorg.clone();
+                has_subscribed.store(true, Ordering::SeqCst);
+                mock_sub.expect_next().returning(move || {
+                    if simul_reorg && height_subscr_counter == block_height_reorg_happens_at {
+                        is_reorg.store(true, Ordering::SeqCst);
+                        info!(
+                            "Reorg initiated at block height {} with hash {}",
                             height_subscr_counter,
-                            block_height_reorg_from,
-                            reorg_active,
-                        )
-                    } else {
-                        generator_clone.generate_block(height_subscr_counter, 0, reorg_active)
-                    };
+                            generator.generate_hash(height_subscr_counter, "alt")
+                        );
+                    }
+                    thread::sleep(Duration::from_millis(delay_between_blocks_subscription));
+                    let block = generator.generate_block(height_subscr_counter);
                     height_subscr_counter += 1;
                     if height_subscr_counter <= block_height_subscription_max {
                         Ok(block)
                     } else {
-                        while !shutdown_flag_clone.is_on() {
-                            thread::sleep(Duration::from_millis(20));
-                        }
+                        shutting_down.set(true);
                         Ok(block)
                     }
-                }
+                });
+                mock_sub.expect_unsubscribe().returning(|| Ok(()));
+                Ok(mock_sub)
             });
-            mock_sub.expect_unsubscribe().returning(|| Ok(()));
-            Ok(mock_sub)
-        }
-    });
+    }
 }
