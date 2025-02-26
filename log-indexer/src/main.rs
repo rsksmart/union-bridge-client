f11be47504121c21e4229e16e9af7ee150674e45
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2025-02-26 14:58:18 +0000

    refactor(log-indexer): extract cli flags to constants

--- log-indexer/src/main.rs
+++ log-indexer/src/main.rs
@@ -5,29 +5,33 @@ use log::{error, info};
 use log_indexer::{indexer::LogIndexer, store::RawLogStore};
 use rsk_provider::rpc::AlloyProvider;
 
+const LOGGER_CLI_FLAG: &str = "logger-path";
+const CONFIG_CLI_FLAG: &str = "config-path";
+
 fn main() -> Result<()> {
     let matches = Command::new("Union Bridge Log Indexer")
         .arg(
-            Arg::new("logger-path")
+            Arg::new(LOGGER_CLI_FLAG)
                 .short('l')
-                .long("logger-path")
+                .long(LOGGER_CLI_FLAG)
                 .value_name("PATH")
-                .help("Sets the path to the log4rs configuration file"),
+                .help("Sets the path to the log4rs configuration file")
+                .default_value("../log4rs.yaml"),
         )
         .arg(
-            Arg::new("config-path")
+            Arg::new(CONFIG_CLI_FLAG)
                 .short('c')
-                .long("config-path")
+                .long(CONFIG_CLI_FLAG)
                 .value_name("PATH")
                 .help("Sets the path to the configuration directory")
+                .default_value("../config/dev/config.yaml"),
         )
         .get_matches();
 
-    let logger_path: &String = matches.get_one("logger-path").unwrap();
-    log4rs::init_file(logger_path, Default::default())
-        .expect("Failed to load log4rs config");
+    let logger_path: &String = matches.get_one(LOGGER_CLI_FLAG).unwrap();
+    log4rs::init_file(logger_path, Default::default()).expect("Failed to load log4rs config");
 
-    let config_path: &String = matches.get_one("config-path").unwrap();
+    let config_path: &String = matches.get_one(CONFIG_CLI_FLAG).unwrap();
     let config = Config::load(config_path).expect("Failed to load config");
 
     let store = RawLogStore::new(&format!("{}/logs", config.indexer.storage.path))?;
