f94f77cef940ba4ec24d3b38210980bc79ff354f
Iago Lluque <iago.lluque@rootstocklabs.com>
2025-03-13 19:49:15 +0100

    refactor(rsk_provider): applied times bound to mock usage (#39)

--- test-utils/src/mock_rsk_provider_handler.rs
+++ test-utils/src/mock_rsk_provider_handler.rs
@@ -66,7 +66,8 @@ impl<'a> MockRskProviderHandler<'a> {
         self.provider
             .expect_get_block_by_hash()
             .with(mockall::predicate::eq(expected_block_hash))
-            .returning(move |_hash| Ok(Some(generator.generate_block(block_height))));
+            .returning(move |_hash| Ok(Some(generator.generate_block(block_height))))
+            .times(1..);
     }
 
     pub fn set_provider_expect_get_best_block(&mut self) {
@@ -74,15 +75,18 @@ impl<'a> MockRskProviderHandler<'a> {
         let has_subscribed = self.has_subscribed.clone();
         let block_height_subscription_max = self.block_height_subscription_max;
         let block_height_backward_sync_max = self.block_height_backward_sync_max;
-        self.provider.expect_get_best_block().returning(move || {
-            // whenever a subscription was activated, the best block is the subscription max
-            let block_height = if has_subscribed.load(Ordering::SeqCst) {
-                block_height_subscription_max
-            } else {
-                block_height_backward_sync_max
-            };
-            Ok(generator.generate_block(block_height))
-        });
+        self.provider
+            .expect_get_best_block()
+            .returning(move || {
+                // whenever a subscription was activated, the best block is the subscription max
+                let block_height = if has_subscribed.load(Ordering::SeqCst) {
+                    block_height_subscription_max
+                } else {
+                    block_height_backward_sync_max
+                };
+                Ok(generator.generate_block(block_height))
+            })
+            .times(1..);
     }
 
     pub fn set_provider_expect_get_block_by_number(
@@ -129,7 +133,8 @@ impl<'a> MockRskProviderHandler<'a> {
                 } else {
                     Ok(None)
                 }
-            });
+            })
+            .times(1..);
     }
 
     pub fn set_provider_expect_subscribe_blocks(
@@ -143,38 +148,44 @@ impl<'a> MockRskProviderHandler<'a> {
         let has_subscribed = self.has_subscribed.clone();
         let delay_between_blocks_subscription = self.delay_between_blocks_subscription;
         let block_height_subscription_max = self.block_height_subscription_max;
-        self.provider.expect_subscribe_blocks().returning(move || {
-            let mut mock_sub = MockRskSubscription::<RskBlock>::new();
-            let generator = generator.clone();
-            let shutting_down = shutting_down.clone();
-            let is_reorg = is_reorg.clone();
-            has_subscribed.store(true, Ordering::SeqCst);
-            mock_sub.expect_next().returning(move || {
-                // if a reorg has to happen and the height is the reorg height, activate the reorg
-                if let Some(reorg_happens_at_height) = simul_reorg_happens_at_height {
-                    if height_subscr_counter == reorg_happens_at_height {
-                        is_reorg.store(true, Ordering::SeqCst);
-                        info!(
-                            "Reorg initiated at block height {} with hash {}",
-                            height_subscr_counter,
-                            generator.generate_hash(height_subscr_counter, "alt")
-                        );
-                    }
-                }
-                thread::sleep(Duration::from_millis(delay_between_blocks_subscription));
-                let block = generator.generate_block(height_subscr_counter);
-                height_subscr_counter = height_subscr_counter + 1;
-                // if the block height passes the subscription max, start shutting down
-                if height_subscr_counter <= block_height_subscription_max {
-                    Ok(block)
-                } else {
-                    shutting_down.set(true);
-                    Ok(block)
-                }
-            });
-            mock_sub.expect_unsubscribe().returning(|| Ok(()));
-            Ok(mock_sub)
-        });
+        self.provider
+            .expect_subscribe_blocks()
+            .returning(move || {
+                let mut mock_sub = MockRskSubscription::<RskBlock>::new();
+                let generator = generator.clone();
+                let shutting_down = shutting_down.clone();
+                let is_reorg = is_reorg.clone();
+                has_subscribed.store(true, Ordering::SeqCst);
+                mock_sub
+                    .expect_next()
+                    .returning(move || {
+                        // if a reorg has to happen and the height is the reorg height, activate the reorg
+                        if let Some(reorg_happens_at_height) = simul_reorg_happens_at_height {
+                            if height_subscr_counter == reorg_happens_at_height {
+                                is_reorg.store(true, Ordering::SeqCst);
+                                info!(
+                                    "Reorg initiated at block height {} with hash {}",
+                                    height_subscr_counter,
+                                    generator.generate_hash(height_subscr_counter, "alt")
+                                );
+                            }
+                        }
+                        thread::sleep(Duration::from_millis(delay_between_blocks_subscription));
+                        let block = generator.generate_block(height_subscr_counter);
+                        height_subscr_counter = height_subscr_counter + 1;
+                        // if the block height passes the subscription max, start shutting down
+                        if height_subscr_counter <= block_height_subscription_max {
+                            Ok(block)
+                        } else {
+                            shutting_down.set(true);
+                            Ok(block)
+                        }
+                    })
+                    .times(1..);
+                mock_sub.expect_unsubscribe().returning(|| Ok(())).times(1);
+                Ok(mock_sub)
+            })
+            .times(1);
     }
 
     pub fn set_provider_expect_subscribe_logs(
@@ -200,18 +211,22 @@ impl<'a> MockRskProviderHandler<'a> {
                 has_subscribed.store(true, Ordering::SeqCst);
                 let mut tuples = tuples.clone();
                 let event_signature = event_signature.clone();
-                mock_sub.expect_next().returning(move || {
-                    generate_next_rsk_log(
-                        delay_between_blocks_subscription,
-                        &log_generator,
-                        &shutting_down,
-                        &mut tuples,
-                        &event_signature,
-                    )
-                });
-                mock_sub.expect_unsubscribe().returning(|| Ok(()));
+                mock_sub
+                    .expect_next()
+                    .returning(move || {
+                        generate_next_rsk_log(
+                            delay_between_blocks_subscription,
+                            &log_generator,
+                            &shutting_down,
+                            &mut tuples,
+                            &event_signature,
+                        )
+                    })
+                    .times(1..);
+                mock_sub.expect_unsubscribe().returning(|| Ok(())).times(1);
                 Ok(mock_sub)
-            });
+            })
+            .times(1);
     }
 
     pub fn set_provider_expect_decode_log(&mut self) {
@@ -231,7 +246,8 @@ impl<'a> MockRskProviderHandler<'a> {
                     ),
                     serde_json::Value::Null,
                 )))
-            });
+            })
+            .times(1..);
     }
 }
 
