8ab6c447fee957d20efabe54d28c1112814f0642
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2025-05-20 14:16:56 +0200

    Check if initial block hash exists before starting block indexer (#77)
    * feat(block-indexer): check if initial block hash exists before starting indexer
    
    * fix: integration tests

--- block-indexer/src/config.rs
+++ block-indexer/src/config.rs
@@ -14,6 +14,7 @@ pub struct Config {
 impl Config {
     pub fn load(base_path: Option<&String>) -> Result<Self, ConfigError> {
         let (cfg, _) = CommonConfig::load_config::<Self>(base_path, CARGO_PKG_NAME)?;
+
         Ok(cfg)
     }
 }
