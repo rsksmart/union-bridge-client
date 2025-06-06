71abf0fb4baebfe3d5d67de115ceb688630f821c
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2025-06-06 18:38:59 +0200

    Make Coordinator proxy received BitVMX event to Transaction Dispatcher Http Server (#83)
    * feat(coordinator): make coordinator proxy received bitvmx event to transaction-dispatcher http server
    
    * feat(coordinator): make try_bitvmx_event return BrokerResponses
    
    * feat(coordinator): create new bitvmx event processor

--- coordinator/src/event_processor.rs
+++ coordinator/src/event_processor.rs
@@ -1,13 +1,26 @@
 use crate::types::RskPegManagerEvents;
 use anyhow::Result;
+use common::{
+    msg_broker::types::BrokerResponses,
+    types::{RskBlock, RskBlockAndUncles},
+};
 
 mod advance_funds;
-
 pub use advance_funds::advance_funds_pegout_processor::*;
-use common::types::RskBlockAndUncles;
+
+mod get_temporary_pegin_address_processor;
+pub use get_temporary_pegin_address_processor::*;
 
 pub trait EventProcessor {
-    fn process_new_event(&mut self, event: &RskPegManagerEvents) -> Result<()>;
+    fn process_new_bitvmx_event(&mut self, _event: &BrokerResponses) -> Result<()> {
+        // default no-op
+        Ok(())
+    }
+
+    fn process_new_event(&mut self, _event: &RskPegManagerEvents) -> Result<()> {
+        // default no-op
+        Ok(())
+    }
 
     fn process_new_block(&mut self, _block: &RskBlockAndUncles) -> Result<()> {
         // default no-op
