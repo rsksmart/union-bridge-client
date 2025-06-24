7064ce3478cbb6f0aa79d5b972070452a3df3ad8
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2025-06-24 08:49:12 +0000

    feat(coordinator): add event data and restructure processor

--- coordinator/src/event_processor/pegin_processor.rs
+++ coordinator/src/event_processor/pegin_processor.rs
@@ -1,9 +1,12 @@
 use crate::{
-    event_processor::EventProcessor,
+    event_processor::{
+        EventProcessor,
+        blockchain_tracker::{BlockConfirmations, BlockchainObserver, BlockchainView},
+    },
     types::{RegisteredPegInRequestEvent, RskPegManagerEvents},
 };
 use anyhow::{Context, Result, bail};
-use bitvmx_client::types::IncomingBitVMXApiMessages;
+use bitvmx_client::{program::variables::VariableTypes, types::IncomingBitVMXApiMessages};
 use common::{
     msg_broker::{
         broker::{BROKER_SERVER_ID, BrokerClientApi},
@@ -13,29 +16,23 @@ use common::{
 };
 use log::info;
 use reqwest::blocking::Client;
-use serde_json::Value;
-use std::env;
+use serde_json::{Value, json};
+use std::{cell::RefCell, env, rc::Rc};
 use uuid::Uuid;
 
 const REGISTERED_PEGIN_REQUEST_CONFIRMATIONS: u32 = 5;
-
-pub struct PeginProcessor<T: BrokerClientApi> {
-    http_client: Client,
-    bitvmx_broker: T,
-    register_pegin_events: Vec<UnconfirmedEvent<RegisteredPegInRequestEvent>>,
-}
 
 #[derive(Debug, Clone)]
 struct UnconfirmedEvent<T: Clone> {
     event_id: Uuid,
     data: T,
-    confirmations: Confirmations,
+    confirmations: BlockConfirmations,
 }
 
 impl<T: Clone> UnconfirmedEvent<T> {
     fn new(data: T, required_confirmations: u32) -> Self {
         let event_id = Uuid::new_v4();
-        let confirmations = Confirmations::new(event_id.to_string(), required_confirmations);
+        let confirmations = BlockConfirmations::new(event_id.to_string(), required_confirmations);
 
         Self {
             event_id,
@@ -44,50 +41,30 @@ impl<T: Clone> UnconfirmedEvent<T> {
         }
     }
 
-    fn register_confirmation(&mut self) {
-        self.confirmations.update(false);
-    }
-
     fn is_confirmed(&self) -> bool {
         self.confirmations.is_confirmed()
     }
 }
 
-#[derive(Debug, Clone)]
-struct Confirmations {
-    flow_id: String,
-    accum: u32,
-    req: u32,
+impl<T: Clone> BlockchainObserver for UnconfirmedEvent<T> {
+    fn get_id(&self) -> String {
+        self.event_id.to_string()
+    }
+
+    fn on_block_added(&mut self, block: &RskBlockAndUncles) {
+        self.confirmations.on_block_added(block);
+    }
+
+    fn on_block_removed(&mut self, block: &RskBlockAndUncles) {
+        self.confirmations.on_block_removed(block);
+    }
 }
 
-impl Confirmations {
-    pub fn new(flow_id: String, req_confirmations: u32) -> Self {
-        Self {
-            flow_id,
-            accum: 0,
-            req: req_confirmations,
-        }
-    }
-
-    pub fn update(&mut self, removed: bool) {
-        if removed {
-            self.accum = self.accum.saturating_sub(1);
-            info!(
-                "Removed confirmation for {}. Status: {}/{}",
-                self.flow_id, self.accum, self.req
-            );
-        } else {
-            self.accum = self.accum.saturating_add(1);
-            info!(
-                "Added confirmation to {}. Status: {}/{}",
-                self.flow_id, self.accum, self.req
-            );
-        }
-    }
-
-    pub fn is_confirmed(&self) -> bool {
-        self.accum >= self.req
-    }
+pub struct PeginProcessor<T: BrokerClientApi> {
+    http_client: Client,
+    bitvmx_broker: T,
+    blockchain: BlockchainView,
+    register_pegin_events: Vec<UnconfirmedEvent<RegisteredPegInRequestEvent>>,
 }
 
 impl<T: BrokerClientApi> PeginProcessor<T> {
@@ -95,6 +72,7 @@ impl<T: BrokerClientApi> PeginProcessor<T> {
         Self {
             http_client: Client::new(),
             bitvmx_broker,
+            blockchain: BlockchainView::new(),
             register_pegin_events: Vec::new(),
         }
     }
@@ -124,16 +102,61 @@ impl<T: BrokerClientApi> PeginProcessor<T> {
             // For now send the result back to bitvmx client mock, in the future
             // this will probably go to the end user
             "pegin-address" => ToServer::TemporaryPegInAddressMockedBitVMX(payload),
-            // TODO: figure out what goes here
-            "register-pegin" => ToServer::ToBitVMX(IncomingBitVMXApiMessages::Ping()),
+            "register-pegin" => {
+                ToServer::ToBitVMX(IncomingBitVMXApiMessages::DispatchTransactionName(
+                    Uuid::new_v4(),
+                    "register-pegin".to_string(),
+                ))
+            }
             _ => bail!("Unsupported method name for BitVMX response: {}", method),
         };
 
         Ok(self.bitvmx_broker.send(BROKER_SERVER_ID, msg)?)
     }
 
+    fn process_confirmed_register_pegin_events(&mut self) -> Result<()> {
+        let mut retained = Vec::new();
+        let events: Vec<_> = self.register_pegin_events.drain(..).collect();
+
+        for event in events {
+            if event.is_confirmed() {
+                let event_id = event.event_id;
+                let data = event.data.inner;
+                self.handle_confirmed_event(event_id, "RegisteredPegInRequest", data)?;
+            } else {
+                retained.push(event);
+            }
+        }
+
+        self.register_pegin_events = retained;
+        Ok(())
+    }
+
+    fn handle_confirmed_event<E: serde::Serialize>(
+        &mut self,
+        event_id: Uuid,
+        variable_name: &str,
+        data: E,
+    ) -> Result<()> {
+        let data = json!(data).to_string();
+
+        self.bitvmx_broker.send(
+            BROKER_SERVER_ID,
+            ToServer::ToBitVMX(IncomingBitVMXApiMessages::SetVar(
+                event_id,
+                variable_name.to_string(),
+                VariableTypes::String(data),
+            )),
+        )?;
+
+        self.blockchain
+            .remove_observer(event_id.to_string().as_str());
+
+        Ok(())
+    }
+
     fn get_tx_dispatcher_url() -> String {
-        // env var because the http server is temporary: defined for docker, defaulting otherwise
+        // Env var because the http server is temporary: defined for docker, defaulting otherwise
         env::var("TRANSACTION_DISPATCHER_URL").unwrap_or("http://0.0.0.0:3000".to_string())
     }
 }
@@ -161,7 +184,7 @@ impl<T: BrokerClientApi> EventProcessor for PeginProcessor<T> {
                     method, result
                 );
             }
-            _ => {} // ignore unrelated events
+            _ => {}
         }
 
         Ok(())
@@ -179,47 +202,35 @@ impl<T: BrokerClientApi> EventProcessor for PeginProcessor<T> {
                     UnconfirmedEvent::new(data.clone(), REGISTERED_PEGIN_REQUEST_CONFIRMATIONS);
 
                 self.register_pegin_events.push(unconfirmed_event.clone());
+                self.blockchain
+                    .add_observer(Rc::new(RefCell::new(unconfirmed_event.clone())));
 
                 info!(
                     "Successfully added RegisteredPegInRequest event to unconfirmed queue. Event: {:?}",
                     unconfirmed_event
                 );
             }
-            _ => {} // ignore unrelated events
+            _ => {}
         }
 
         Ok(())
     }
 
-    fn process_new_block(&mut self, _block_with_uncles: &RskBlockAndUncles) -> Result<()> {
-        // TODO: handle a reorg case, we should remove from queue?
-
+    fn process_new_block(&mut self, block: &RskBlockAndUncles) -> Result<()> {
         if self.register_pegin_events.is_empty() {
             return Ok(());
         }
 
-        let mut retained = Vec::new();
+        self.blockchain.update(block.clone());
 
-        for mut event in self.register_pegin_events.drain(..) {
-            event.register_confirmation();
-
-            if event.is_confirmed() {
-                // TODO: Replace with actual message, figure out what goes inside Setup
-                self.bitvmx_broker.send(
-                    BROKER_SERVER_ID,
-                    ToServer::ToBitVMX(IncomingBitVMXApiMessages::Ping()),
-                )?;
-            } else {
-                retained.push(event);
-            }
-        }
-
-        self.register_pegin_events = retained;
+        self.process_confirmed_register_pegin_events()?;
 
         Ok(())
     }
 
     fn shutdown(&mut self) {
         info!("Shutting down GetTemporaryPeginAddressProcessor");
+
+        self.blockchain.clear();
     }
 }
