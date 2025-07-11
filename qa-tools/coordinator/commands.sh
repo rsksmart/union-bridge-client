761f85aca55e0c5c52ab75c52496c6163d057cd6
F Javier Aguerri <javier.aguerri@rootstocklabs.com>
2025-07-11 18:26:10 +0200

    Initial testing of coordinator (#118)
    * test: uncomment feature user stories
    
    * test: anvil proxy to inject difficulty in blocks
    
    * test: unit test - coordinator hanles incoming blocks in wrong order
    
    * test: manual scenarios - coordinator - advance funds
    
    * test: adapt anvil_proxy to target 8545 - improve config setup for coordinator tests
    
    * test: last improvements

---
+++ qa-tools/coordinator/commands.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# RPC port
+RPC_PORT=8546
+RPC_URL=http://127.0.0.1:$RPC_PORT
+
+# Get current block number
+function current_block() {
+  local block_hex=$(curl -s -X POST -H "Content-Type: application/json" \
+    -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' \
+    $RPC_URL | jq -r '.result')
+  local block_dec=$((block_hex))
+  echo "Current block number: $block_dec"
+}
+
+# Load evm snapshot
+function load() {
+  echo "[evm_revert]"
+  curl -s -X POST -H "Content-Type: application/json" \
+    -d '{"jsonrpc":"2.0","method":"evm_revert","params":["0x0"],"id":1}' \
+    $RPC_URL | jq
+  current_block
+}
+
+# Save evm snapshot
+function save() {
+  echo "[evm_snapshot]"
+  curl -s -X POST -H "Content-Type: application/json" \
+    -d '{"jsonrpc":"2.0","method":"evm_snapshot","params":[],"id":1}' \
+    $RPC_URL | jq
+  current_block
+}
+
+# Mine a block
+function mine() {
+  echo "[evm_mine]"
+  curl -s -X POST -H "Content-Type: application/json" \
+    -d '{"jsonrpc":"2.0","method":"evm_mine","params":[],"id":1}' \
+    $RPC_URL | jq
+  current_block
+}
+
+# Fund an account
+function fund() {
+  echo "[cast send]"
+  cast send \
+    --rpc-url $RPC_URL \
+    --from 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 \
+    0x5bdd03ceaf59cad075cb29c67696581d857b9031 \
+    --value 1ether \
+    --unlocked
+  current_block
+}
+
+# Export functions into current shell
+export -f save load mine fund current_block
+echo "Commands loaded: save, load, mine, fund"
