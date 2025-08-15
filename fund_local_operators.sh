ee71eb84592e688e81c4d5c6163b19e956936370
Iago Lluque <iago.lluque@rootstocklabs.com>
2025-08-15 20:19:45 +0200

    build: git-ignored fund_local_operators.sh

--- fund_local_operators.sh
+++
@@ -1,7 +0,0 @@
-cast send --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80   <OPERATOR_1_ADDRESS>   --value 1000000000000000000   --rpc-url http://127.0.0.1:8545
-sleep 2
-cast send --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80   <OPERATOR_2_ADDRESS>   --value 1000000000000000000   --rpc-url http://127.0.0.1:8545
-sleep 2
-cast send --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80   <OPERATOR_3_ADDRESS>   --value 1000000000000000000   --rpc-url http://127.0.0.1:8545
-sleep 2
-cast send --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80   <OPERATOR_4_ADDRESS>   --value 1000000000000000000   --rpc-url http://127.0.0.1:8545
