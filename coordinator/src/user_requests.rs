9f8695eec614cefe2062c76486d1719e2297f7a0
Iago Lluque <iago.lluque@rootstocklabs.com>
2025-08-27 16:57:13 +0200

    Feat/committee registry utxos otf regtest (#176)
    * refactor(committee-registry): generate UTXOs on demand for regtest
    
    * refactor(committee-registry): simplified user input on ApplyToStream now that the UTXOs are generated on the fly

--- coordinator/src/user_requests.rs
+++ coordinator/src/user_requests.rs
@@ -1,21 +1,22 @@
-use anyhow::Context;
 use common::msg_broker::bitvmx_types::ParticipantRole;
 use common::types::StreamId;
 use serde::{Deserialize, Serialize};
-use union_contracts::bindings::committee_registry::CommitteeRegistry::UTXO;
 // TODO create types mod and move this and types.rs (renamed to rsk_events.rs) there
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct ApplyToStream {
     pub stream_id: StreamId, // Matches StreamDenomination in the contract
     pub role: Role,
-    pub utxo: Utxo,
+    pub funding_utxo: Utxo,
+    pub speed_up_utxo: Utxo,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Utxo {
-    pub txid: String,
-    pub vout: u32,
+    // temporarily omitted for Regtest stage
+    // pub txid: String,
+    // temporarily omitted for Regtest stage
+    // pub vout: u32,
     pub value: u64,
 }
 
@@ -25,20 +26,21 @@ pub enum Role {
     Verifier,
 }
 
-impl TryFrom<Utxo> for UTXO {
-    type Error = anyhow::Error;
-
-    fn try_from(utxo: Utxo) -> Result<Self, Self::Error> {
-        Ok(UTXO {
-            txid: utxo
-                .txid
-                .parse()
-                .context("Failed to parse String txid to FixedBytes<32>")?,
-            outputIndex: utxo.vout,
-            amount: utxo.value,
-        })
-    }
-}
+// temporarily omitted for Regtest stage
+// impl TryFrom<Utxo> for UTXO {
+//     type Error = anyhow::Error;
+//
+//     fn try_from(utxo: Utxo) -> Result<Self, Self::Error> {
+//         Ok(UTXO {
+//             txid: utxo
+//                 .txid
+//                 .parse()
+//                 .context("Failed to parse String txid to FixedBytes<32>")?,
+//             outputIndex: utxo.vout,
+//             amount: utxo.value,
+//         })
+//     }
+// }
 
 impl From<Role> for ParticipantRole {
     fn from(role: Role) -> Self {
