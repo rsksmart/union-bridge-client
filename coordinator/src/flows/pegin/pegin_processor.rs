1930bfeb7dbf6da92d4fe6359c56896f0cfa1071
Iago Lluque <iago.lluque@rootstocklabs.com>
2025-12-19 18:52:01 +0100

    chore: update debug logging to specify Rootstock confirmations in event processors

--- coordinator/src/flows/pegin/pegin_processor.rs
+++ coordinator/src/flows/pegin/pegin_processor.rs
@@ -840,7 +840,7 @@ where
 
             self.events_confirming.insert(confirmable_event.id(), confirmable_event);
 
-            debug!("Waiting for confirmations for {id}");
+            debug!("Waiting Rootstock confirmations for {id}");
         }
 
         Ok(())
