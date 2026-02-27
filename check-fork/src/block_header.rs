5c7687c19e2d08881ea6a0df930c0a289316d443
Federico Jinich <federico@rootstocklabs.com>
2026-02-27 10:26:36 -0300

    refactor(check-fork): prevent panic on deserializers

--- check-fork/src/block_header.rs
+++ check-fork/src/block_header.rs
@@ -128,7 +128,7 @@ where
     let s: String = Deserialize::deserialize(deserializer)?;
     let s = s.strip_prefix("0x").unwrap_or(&s);
     let bytes = hex::decode(s).map_err(serde::de::Error::custom)?;
-    Ok(H256::from_slice(&bytes))
+    from_bytes_vec_to_h256(bytes)
 }
 
 pub fn deserialize_hex_bytes<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
@@ -168,9 +168,20 @@ where
         .map(|s| {
             let s = s.strip_prefix("0x").unwrap_or(s);
             let bytes = hex::decode(s).map_err(serde::de::Error::custom)?;
-            Ok(H256::from_slice(&bytes))
+            from_bytes_vec_to_h256(bytes)
         })
         .collect()
+}
+
+fn from_bytes_vec_to_h256<E>(bytes: Vec<u8>) -> Result<H256, E>
+where
+    E: serde::de::Error,
+{
+    if bytes.len() != 32 {
+        return Err(serde::de::Error::custom(format!("Expected 32 bytes, got {}", bytes.len())));
+    }
+
+    Ok(H256::from_slice(&bytes))
 }
 
 pub fn deserialize_hex_bytes_20<'de, D>(deserializer: D) -> Result<[u8; 20], D::Error>
