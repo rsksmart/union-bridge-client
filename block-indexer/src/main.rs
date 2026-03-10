e5fcbb28ab06c6d0f8e160871a79f7ba11d3222b
LordCheta <chinaechetam.maduabuchi@rootstocklabs.com>
2026-03-09 21:53:18 -0700

    fix: remove stale initial_block_hash parsing from rebase conflict
    The block was from an older commit; main moved this logic into
    IndexerConfig::resolve_initial_block() in common/src/config.rs.

--- block-indexer/src/main.rs
+++ block-indexer/src/main.rs
@@ -10,7 +10,7 @@ use common::alloy_rsk_provider::rpc::AlloyProvider;
 use common::msg_broker::broker::BrokerServer;
 use common::rsk_indexer::RskIndexer;
 use common::shutdown_flag::ShutdownFlag;
-use common::types::{BlockHash, RskBlockAndUncles};
+use common::types::RskBlockAndUncles;
 use log::{debug, error, info};
 
 const LOGGER_CLI_FLAG: &str = "logger-path";
@@ -51,11 +51,6 @@ fn main() -> Result<()> {
 
     let alloy_provider = AlloyProvider::new(&config.provider.rootstock.url, shutdown_flag.clone())
         .expect("Failed to create AlloyProvider (unrecoverable)");
-
-    let initial_block_hash = BlockHash::try_from(config.indexer.initial_block_hash.as_str())
-        .unwrap_or_else(|_| {
-            panic!("Invalid initial block hash: {}", config.indexer.initial_block_hash)
-        });
 
     // TODO(Jira) UB-132 - think about bounding the channel
     let (tx, rx): (mpsc::Sender<RskBlockAndUncles>, mpsc::Receiver<RskBlockAndUncles>) =
