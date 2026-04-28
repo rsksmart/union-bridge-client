250b128ea682535a94413026f7c9a3b1a269b22d
Iago Lluque <iago.lluque@rootstocklabs.com>
2026-04-28 15:26:49 +0200

    fix: add ANSI stripping to Docker log capture for cleaner output

--- scripts/backup-local-logs.sh
+++ scripts/backup-local-logs.sh
@@ -44,14 +44,18 @@ print_capture_result() {
   fi
 }
 
+strip_ansi() {
+  perl -pe 's/\e\[[0-?]*[ -\/]*[@-~]//g'
+}
+
 capture_command() {
   local label="$1"
   local subject="$2"
   local destination_file="${log_dir}/${label}.log"
   shift 2
 
-  "$@" > "${destination_file}" 2>&1
-  local rc=$?
+  "$@" 2>&1 | strip_ansi > "${destination_file}"
+  local rc=${PIPESTATUS[0]}
   print_capture_result "${subject}" "${rc}"
 }
 
